5c46393f6c8f0bac755f107aec41bd3aaee60e13
ColKim <ColleenArtKim@gmail.com>
2018-04-21 14:36:24 -0400

    color
    color var

--- draw.js
+++ draw.js
@@ -8,12 +8,30 @@ var drawing = (function(){
 		moveY = [],
 		moveDrag = [],
 		paint = false,
+		// Colors
+		colorRed = "#FF3232",
+        colorGreen = "00E164",
+        colorBlue = "0064E1",
+        curColor = colorRed;,
+        clickColor = new Array();,
 
 		addClick = function (x,y,dragging){
 			moveX.push(x);
 			moveY.push(y);
 			moveDrag.push(dragging);
+			clickColor.push(curColor);
+
+
+		},
+
+		clearCanvas = function() {
+		    context.clearRect(0,0,canvasWidth, canvasHeight)
+		},
+
+		redrew = function() {
+
 		}
+
 	init = function () {
 		canvas = document.createElement('canvas');
 		canvas.setAttribute('width', canvasWidth);
