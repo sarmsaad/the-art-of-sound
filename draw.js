743eabf207f6f2354e7a5207ebf58801fca3d068
kzhang21 <kellyzhang2197@gmail.com>
2018-04-21 14:09:56 -0400

    js

---
+++ draw.js
@@ -0,0 +1,11 @@
+var drawing = (function(){
+	init = function () {
+		canvas = document.createElement('canvas');
+		canvas.setAttribute('width', canvasWidth);
+		canvas.setAttribute('height', canvasHeight);
+
+	};
+	return {
+		init:int
+	};
+}());
