850918ebc58c15079603b7e028bffae81295497a
kzhang21 <kellyzhang2197@gmail.com>
2018-04-21 14:52:04 -0400

    redraw func

--- draw.js
+++ draw.js
@@ -138,3 +138,22 @@ function addClick(x, y, dragging)
     clickDrag.push(dragging);
 }
 
+function redraw(){
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+
+    for(var i=0; i < clickX.length; i++) {
+        context.beginPath();
+        if(clickDrag[i] && i){
+            context.moveTo(clickX[i-1], clickY[i-1]);
+        }else{
+            context.moveTo(clickX[i]-1, clickY[i]);
+        }
+        context.lineTo(clickX[i], clickY[i]);
+        context.closePath();
+        context.stroke();
+    }
+}
