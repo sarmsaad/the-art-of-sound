0b8f5861add9759180c3f9beb12d3fd0e170c405
kzhang21 <kellyzhang2197@gmail.com>
2018-04-21 14:20:56 -0400

    updated js

--- draw.js
+++ draw.js
@@ -1,8 +1,27 @@
 var drawing = (function(){
+
+	var canvas,
+		context,
+		canvasWidth = 600,
+		canvasHeight = 600,
+		moveX = [],
+		moveY = [],
+		moveDrag = [],
+		paint = false,
+
+		addClick = function (x,y,dragging){
+			moveX.push(x);
+			moveY.push(y);
+			moveDrag.push(dragging);
+		}
 	init = function () {
 		canvas = document.createElement('canvas');
 		canvas.setAttribute('width', canvasWidth);
 		canvas.setAttribute('height', canvasHeight);
+		canvas.setAttribute('id', 'canvas');
+		document.getElementById('canvass').appendChild(canvas);
+
+		context = canvas.getContext("2d");
 
 	};
 	return {
