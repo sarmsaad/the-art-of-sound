4cc9f6e70c97e82acf836930b0a0fea0e397da79
Zui <zuikng@gmail.com>
2018-04-22 11:25:40 -0400

    Add volumn

--- aos/src/router/index.js
+++ aos/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Fun from '@/components/Fun'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: "/fun",
+      name: 'Fun',
+      component: Fun
+
     }
   ]
 })
